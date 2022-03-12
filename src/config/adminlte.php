55384750f553942dc9173312eda0d2d42c46c529
Adriano Maciel <adriano_mail@hotmail.com>
2022-03-12 19:09:25 -0300

    Customizado logo

--- src/config/adminlte.php
+++ src/config/adminlte.php
@@ -46,7 +46,7 @@ return [
     */
 
     'logo' => '<b>Sing</b>MANAUS',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo_img' => 'vendor/adminlte/dist/img/painel-logo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
