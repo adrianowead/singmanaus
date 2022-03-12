272e06963e9054e06beb538fdaf844a8fdca3cc8
Adriano Maciel <adriano_mail@hotmail.com>
2022-03-12 18:28:01 -0300

    Customizando AdminLTE

--- src/config/adminlte.php
+++ src/config/adminlte.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 3',
+    'title' => 'Sing Manaus',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -45,12 +45,12 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
+    'logo' => '<b>Sing</b>MANAUS',
     'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'AdminLTE',
+    'logo_img_alt' => 'SingMANAUS',
 
     /*
     |--------------------------------------------------------------------------
@@ -121,7 +121,7 @@ return [
     |
     */
 
-    'classes_body' => '',
+    'classes_body' => 'dark-mode',
     'classes_brand' => '',
     'classes_brand_text' => '',
     'classes_content_wrapper' => '',
